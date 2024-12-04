bcb99aa68e629e6a8592712adc5bea359ecb2021
Jacob Feng <peanutchowder@gmail.com>
2024-12-04 13:36:59 -0700

    Initial Commit

---
+++ Eye Exam/Eye_ExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eye_ExamApp.swift
+//  Eye Exam
+//
+//  Created by Jacob Feng on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Eye_ExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Eye ExamTests/Eye_ExamTests.swift
@@ -0,0 +1,17 @@
+//
+//  Eye_ExamTests.swift
+//  Eye ExamTests
+//
+//  Created by Jacob Feng on 12/4/24.
+//
+
+import Testing
+@testable import Eye_Exam
+
+struct Eye_ExamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
